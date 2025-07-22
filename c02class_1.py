c0aeff81ee2f490aafa0795de5aed8cd71b4d7ef
V <221497160+Phakinza007@users.noreply.github.com>
2025-07-22 16:21:51 +0700

    exam

---
+++ c02class_1.py
@@ -0,0 +1,27 @@
+x = int(input("entert your km :"))
+
+
+result = 0
+if (x >=5 and x <=50):
+    result = 10
+    print(result)
+elif (x >=51 and x <=100):
+    result = 15
+    print(result)
+elif (x >=101 and x <=300):
+    result = 25
+    print(result)
+elif (x >=301 and x <=500):
+    result = 35
+    print(result)
+elif (x >500):
+    result = 45
+    print(result)
+else :
+    print("free")
+
+vat = result * 0.07
+bath = result + result * 0.07
+print("vat = ",vat)
+print("km =",x)
+print("bath =",bath)
